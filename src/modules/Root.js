9c8b07492920dc4926585a989645568203f0092a
Hana <hana_369@hotmail.com>
2020-02-18 06:33:31 +0530

    Routes are restricted, Toolbar is added, Logout function implemented.

--- src/modules/Root.js
+++ src/modules/Root.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Auth, Toolbar } from './components';
 import { Home, SignIn, SignUp } from './screens';
 import { authSuccess } from './store/actions/auth';
+import Admin from './components/Admin';
 
 import styles from './Root.module.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+
 
 const Root = props => {
   useEffect(() => {
@@ -22,6 +25,7 @@ const Root = props => {
   return (
     <div className={styles.container}>
       <Router>
+        <Toolbar />
         <Switch>
           <Route exact path='/'>
             <Home />
@@ -31,6 +35,9 @@ const Root = props => {
           </Route>
           <Route exact path='/register'>
             <SignUp />
+          </Route>
+          <Route path='/admin' exact>
+            <Auth component={Admin} auth={props.auth} role="ROLE_ADMIN" />
           </Route>
         </Switch>
       </Router>
--- src/modules/store/actions/auth.js
+++ src/modules/store/actions/auth.js
@@ -1,8 +1,14 @@
-import { AUTH_SUCCESS } from './actionTypes';
+import { AUTH_SUCCESS, LOGOUT_SUCCESS } from './actionTypes';
 
 export const authSuccess = payload => {
   return {
     type: AUTH_SUCCESS,
     payload
+  }
+}
+
+export const logoutSuccess = () => {
+  return {
+    type: LOGOUT_SUCCESS
   }
 }
