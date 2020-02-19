5353434b760c9d973af7d3948762ae366ceee2b2
Hana <hana_369@hotmail.com>
2020-02-20 00:35:22 +0530

    Vehicle Item

--- src/modules/components/index.js
+++ src/modules/components/index.js
@@ -3,3 +3,6 @@ export { default as Auth } from './auth/Auth';
 export { default as InputField } from './inputField/InputField';
 export { default as UtilityItem } from './utility/utilityItem/UtilityItem';
 export { default as AddUtility } from './utility/addUtility/AddUtility';
+export { default as VehicleItem } from './vehicle/vehicleItem/VehicleItem';
+export { default as VehicleDetails } from './vehicle/vehicleDetails/VehicleDetails';
+export { default as AddVehicle } from './vehicle/addVehicle/AddVehicle';
