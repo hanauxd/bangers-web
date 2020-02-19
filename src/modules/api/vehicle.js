2ae32e1767eb47f064696b63e1bb4c9e339dcc10
Hana <hana_369@hotmail.com>
2020-02-20 00:34:10 +0530

    Add Vehicle.

--- src/modules/api/vehicle.js
+++ src/modules/api/vehicle.js
@@ -5,8 +5,17 @@ export const fetchVehicles = token => {
   return get(endpoint, token);
 }
 
-export const addVehicle = (values, token) => {
+export const fetchVehicleDetails = id => {
+  const endpoint = `vehicles/${id}`;
+  return get(endpoint);
+}
+
+export const addVehicle = (vehicle, files, token) => {
   const endpoint = "vehicles";
-  const body = { ...values };
-  return post(endpoint, body, token);
+  let formData = new FormData();
+  formData.append('vehicle', JSON.stringify(vehicle));
+  files.forEach(file => {
+    formData.append('file', file);
+  })
+  return post(endpoint, formData, token);
 }
--- src/modules/screens/index.js
+++ src/modules/screens/index.js
@@ -3,4 +3,3 @@ export { default as SignIn } from './signIn/SignIn';
 export { default as SignUp } from './signUp/SignUp';
 export { default as Utility } from './admin/utility/Utility';
 export { default as Vehicle } from './admin/vehicle/Vehicle';
-export { default as AddVehicle } from './admin/vehicle/addVehicle/AddVehicle';
