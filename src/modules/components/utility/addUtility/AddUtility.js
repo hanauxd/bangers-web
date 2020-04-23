f90eca1502a266d5cd5251537bec2406b3863614
Fathimath Hanau <hanau.xd@gmail.com>
2020-04-24 03:31:18 +0530

    Restyle add new utility form

--- src/modules/components/utility/addUtility/AddUtility.js
+++ src/modules/components/utility/addUtility/AddUtility.js
@@ -7,20 +7,18 @@ import { InputField } from "../../index";
 
 import styles from "./AddUtility.module.css";
 
-const AddUtility = props => {
+const AddUtility = (props) => {
     const initialValues = {
         utilityType: "",
-        quantity: ""
+        quantity: "",
     };
 
     const utilitySchema = Yup.object().shape({
         utilityType: Yup.string().required("Utility type is required."),
-        quantity: Yup.number()
-            .min(1, "Quantity cannot be less than 1.")
-            .required("Quantity is required.")
+        quantity: Yup.number().min(1, "Quantity cannot be less than 1.").required("Quantity is required."),
     });
 
-    const handleAddUtility = values => {
+    const handleAddUtility = (values) => {
         props.handleAddUtility(values);
     };
 
@@ -29,28 +27,40 @@ const AddUtility = props => {
             {({ values, handleChange, handleBlur }) => {
                 return (
                     <div className={styles.container}>
-                        <h3>ADD UTILITY</h3>
+                        <h3>Add Utility</h3>
+                        <hr />
                         <Form>
-                            <InputField
-                                label="Utility Type"
-                                type="text"
-                                name="utilityType"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                values={values.utilityType}
-                            />
-                            <InputField
-                                label="Quantity"
-                                type="number"
-                                name="quantity"
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                values={values.quantity}
-                            />
-                            <div className={styles.button__div}>
-                                <MDBBtn block color="mdb-color darken-3" type="submit">
-                                    SAVE
-                                </MDBBtn>
+                            <div className={styles.form__div}>
+                                <InputField
+                                    styleClass={{ flex: 1 }}
+                                    label="Utility Type"
+                                    type="text"
+                                    name="utilityType"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    values={values.utilityType}
+                                />
+                                <div className={styles.separator} />
+                                <InputField
+                                    styleClass={{ flex: 1 }}
+                                    label="Quantity"
+                                    type="number"
+                                    name="quantity"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    values={values.quantity}
+                                />
+                                <div className={styles.separator} />
+                                <div>
+                                    <MDBBtn
+                                        color="mdb-color darken-3"
+                                        size="sm"
+                                        type="submit"
+                                        style={{ margin: "0px" }}
+                                    >
+                                        SAVE
+                                    </MDBBtn>
+                                </div>
                             </div>
                         </Form>
                     </div>
